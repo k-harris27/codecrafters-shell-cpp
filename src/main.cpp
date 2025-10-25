9dbaf85b14345fcab4f42c4ed7fbd21c038f4b0e
k-harris27 <120191386+k-harris27@users.noreply.github.com>
2025-10-25 18:22:22 +0100

    Add type command

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <vector>
+
+const std::vector<std::string> BUILTINS {"exit", "echo", "type"};
+
+void print_command_not_found(std::string& command) {
+  std::cout << command << ": command not found" << std::endl;
+}
 
 int main() {
   while (true) {
@@ -26,8 +33,18 @@ int main() {
     else if (command == "echo") {
       std::cout << input_stream.rdbuf() << std::endl;  // Print the remainder of the input string.
     }
+    else if (command == "type") {
+      std::string args;
+      std::getline(input_stream, args);
+      if (std::find(BUILTINS.begin(), BUILTINS.end(), args) != BUILTINS.end()) {
+        std::cout << args << " is a shell builtin" << std::endl;
+      }
+      else {
+        print_command_not_found(args);
+      }
+    }
     else {
-      std::cout << input << ": command not found" << std::endl;
+      print_command_not_found(command);
     }
   }
 }
