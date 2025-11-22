7d8df75173cf387274b247f13b2a1d20c0271e04
k-harris27 <120191386+k-harris27@users.noreply.github.com>
2025-11-22 17:36:43 +0000

    Add ~ expansion to cd builtin

--- src/builtins/cd.cpp
+++ src/builtins/cd.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <string>
 #include <filesystem>
+#include <algorithm>
+#include <unistd.h>
 
 #include "builtins.hpp"
 
@@ -19,6 +21,21 @@ BuiltinResult BuiltinCd(std::vector<std::string> &tokens)
   if (ntokens == 2)
   {
     target = tokens[1];
+
+    auto target_str = target.string();
+    if (target_str[0] == '~')
+    {
+      std::filesystem::path home_path = std::getenv("HOME");
+      if (target_str.length() > 1)
+      {
+        target = home_path / std::string_view(target.c_str()).substr(2);
+      }
+      else
+      {
+        target = home_path;
+      }
+    }
+
     if (!std::filesystem::exists(target))
     {
       std::cout << "cd: " << target.string() << ": No such file or directory" << std::endl;
